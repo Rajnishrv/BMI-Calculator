be7ca40c954f2abd6a2d53f69de559ad5e7d77c7
Rajneesh kumar <rajnishkumar550443@gmail.com>
2024-02-03 22:36:08 +0530

    add js for change background color

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit',(e)=>{
   e.preventDefault();
 
   const height = parseInt(document.querySelector('#height').value);
@@ -14,6 +14,19 @@ form.addEventListener('submit', function (e) {
   }
   else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2)
-    results.innerHTML = `<span>${bmi}</span>`
+    results.innerHTML = `<span>BMI Result is: ${bmi}</span>`
+
+    const fresult = document.querySelector('#fresult')
+  
+    if(bmi < 18.6){
+      fresult.innerHTML = "You are under weight";
+      fresult.style.backgroundColor = "yellow";
+    }else if(bmi > 18.6 && bmi <24.9){
+      fresult.innerHTML = "You are Normal";
+      fresult.style.backgroundColor = "green"
+    }else{
+      fresult.innerHTML = `You are Overweight`;
+      fresult.style.backgroundColor = "red"
+    }
   }
 });
